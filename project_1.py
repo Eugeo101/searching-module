96d9630421c7d871f1a5b59327ec20ab7fe7d277
Ahmed Ayman <ahmedayman58134@gmail.com>
2024-04-08 22:09:58 +0200

    adds search feature

--- project_1.py
+++ project_1.py
@@ -1,3 +1,12 @@
 print("Ahmed")
 print("Ibrahim")
 print("Fakhr")
+
+def search_for_a_number(numbers, num=0):
+    counter = 0
+    indices = []
+    for i in range(len(numbers)):
+        if numbers[i] == num:
+            counter += 1
+            indices.append(i)
+    return indices, counter
